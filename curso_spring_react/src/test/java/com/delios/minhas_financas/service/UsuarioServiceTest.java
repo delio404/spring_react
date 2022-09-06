d56e0bfcc1fc41bfd7aaf1f2e96b28045c9b715f
delio404 <adelio.a.castro@gmail.com>
2022-09-06 11:33:57 -0300

    testando metodo de autenticacao de usuario (mock)

--- curso_spring_react/src/test/java/com/delios/minhas_financas/service/UsuarioServiceTest.java
+++ curso_spring_react/src/test/java/com/delios/minhas_financas/service/UsuarioServiceTest.java
@@ -5,14 +5,18 @@ import com.delios.minhas_financas.model.entity.Usuario;
 import com.delios.minhas_financas.repository.UsuarioRepository;
 import com.delios.minhas_financas.services.UsuarioService;
 import com.delios.minhas_financas.services.UsuarioServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 
 @RunWith(SpringRunner.class)
@@ -20,14 +24,33 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class UsuarioServiceTest {
 
     UsuarioService service;
+
+    @MockBean
     UsuarioRepository repository;
 
     @Before
     public void setup(){
-        repository = Mockito.mock(UsuarioRepository.class);
         service=new UsuarioServiceImpl(repository);
     }
 
+    @Test(expected = Test.None.class)
+    public void deveAutenticarUmUsuarioComSucesso(){
+        //cenario
+        String email= "email@email.com";
+        String senha= "senha";
+        Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+        Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+
+        //acao
+        Usuario result = service.autenticar(email,senha);
+
+        //verificacao
+        Assertions.assertThat(result).isNotNull();
+
+
+
+
+    }
 
     @Test(expected = Test.None.class )
     public void deveValidarEmail() {
